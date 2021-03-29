ac0908cf396c51b67aaab1fbcc903c91769baa7f
Gianpaolo <pao.barga@gmail.com>
2021-03-29 18:24:40 +0200

    aggiunto view, controller e route per posts

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,6 +4,25 @@
 
 @section('main-content')
 
-
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Body</th>
+            <th scope="col">Author</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->body }}</td>
+                    <td>{{ $post->author->name}} {{$post->author->surname}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
